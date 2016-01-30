39f5446f78ce9aef09759092df834f47583e28fd
Ashiq Uz Zoha <ashiq.ayon@gmail.com>
2016-01-31 02:59:36 +0600

    comment push fix

--- common/models/event-comment.js
+++ common/models/event-comment.js
@@ -113,7 +113,10 @@ module.exports = function(EventComment) {
 						}, function(err, acnt) {
 							if (acnt[0]) {
                                 console.log("commenter = " + data.AccountId + ", owner = " + business[0].AccountId);
-                                if (data.AccountId === business[0].AccountId) {
+                                console.log("commenter type = " + typeof data.AccountId);
+                                console.log("owner type = " + typeof business[0].AccountId);
+                                
+                                if (data.AccountId == business[0].AccountId) {
                                    console.log("comment from owner");
                                 } else {
                                     message.text = 'New comment from ' + acnt[0].FirstName + " in " + business[0].Name ;
