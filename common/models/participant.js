a1be4f8db4ce2bb0a785ffc70e43e7c6b786a149
Ashiq Uz Zoha <ashiq.ayon@gmail.com>
2016-01-31 11:26:16 +0600

    comment push fix

--- common/models/participant.js
+++ common/models/participant.js
@@ -8,47 +8,34 @@ module.exports = function(Participant) {
 		
         console.log('after remote: perticipant create');
         console.log('perticipant create data = ' + JSON.stringify(data));
-		if(ctx.args.data.Rsvp!=1)
-		Participant.app.models.Installation.find({where:{'userId': ctx.args.data.AccountId}},
-                function(err,device) {
-       				if(err)
-    					done();
-
-					console.log(device[0]);
-					var notification = Participant.app.models.Notification;
-					device[0].badge++;
-					var message={
-					 	EventId: ctx.args.data.EventId
-					 }
-
-		Participant.app.models.Event.find({where:{'id':ctx.args.data.EventId}},function(err,result){
-			if(result[0])
-			Participant.app.models.Account({where:{'id':result[0].AccountId}},function(err,owner) {
-				message.text = owner[0].FirstName + ' has invited you to his Event named '+ result[0].Name;
-			});
-		});			
 		
-		 var note = new notification({
-				   expirationInterval: 3600, // Expires 1 hour from now.
-				   badge: device[0].badge,
-				   sound: 'ping.aiff',
-				   message:message,
-				   messageFrom: 'Even3co'
-				 });
-		 	if(device[0])
-		      Participant.app.models.Push.notifyById(device[0].id, note, function(err) {  
-				   if (err) {
-				     console.log(err);
-				     done();
-				   }
-				   
-                   if(!err) {
-				   	 device[0].save();
-				     console.log('pushing notification to %j', device[0].id);
-				     done();
-				   }
-		    });
-		});
+        Participant.app.models.Account.find({
+            where:{'id' : ctx.args.data.AccountId}
+        }, function (error, users) {
+            if (users.length > 0) {
+                var sender = users[0] ;
+                Participant.app.models.Event.find({
+                    where:{'id':ctx.args.data.EventId}
+                },function(err,result) {                
+                    var event = result[0];
+			        if(event) {
+                        Participant.app.models.Account({
+                            where:{'id':result[0].AccountId}
+                        },function(err,owner) {
+                            var message = {
+                                   EventId : ctx.args.data.EventId
+                            };
+                            if (ctx.args.data.Rsvp == 1) {
+                                message.text = sender.FirstName + ' is not going to your event named ' + event.Name ;
+                            } else if (ctx.args.data.Rsvp == 2) {
+                                message.text = sender.FirstName + ' is going to your event named ' + event.Name ;
+                            }
+				            Participant.app.models.Push.sendNotification(owner.id, message); 
+			            });    
+                    }
+		        });	
+            }
+        });
 	});
 
 
